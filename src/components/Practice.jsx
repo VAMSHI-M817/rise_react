c23db7b88bbf448f2f9f1d7107db9490dcfd6a2d
VAMSHI-M817 <vamshimarepu817@gmail.com>
2024-06-25 00:30:01 +0530

    hooks updated

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { useState } from "react"
+
 
 const Practice = () => {
+  const [firstName, setfirstName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const Obj = {
+      firstName: firstName,
+      email: email,
+      password: password
+    }
+    console.log(Obj)
+  }
+
+
   return (
-    <div>Practice</div>
+    <div>
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <div>
+          <input type="text"
+            id="firstName"
+            name="firstName"
+            value={firstName}
+            placeholder="Enter your name"
+            onChange={(e) => setfirstName(e.target.value)}
+          />
+        </div>
+        <div>
+          <input type="email"
+            id="email"
+            name="email"
+            value={email}
+            placeholder="Enter your email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <input type="password"
+            id="password"
+            name="password"
+            value={password}
+            placeholder="Enter your password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
+
+
 
 export default Practice
